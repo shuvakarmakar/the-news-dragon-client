56564156deb5234d82e4d9d4be61b5f02590150e
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-04-28 13:54:10 +0600

    NavBar and Header Work Done and I used React Marquee and Moment Js for Time

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -2,16 +2,43 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
-        </div>
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-danger' speed={60}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+            {/* For navbar */}
+            <Navbar collapseOnSelect expand="lg" variant="light">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <Button variant="dark">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 };
 
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const RightNav = () => {
     return (
         <div>
-            <h2>This is Roght Nav</h2>
+            <h4>Login With</h4>
+            <Button variant="outline-primary">Login With Google</Button>
+            <Button variant="outline-secondary">Login With GitHub</Button>
         </div>
     );
 };
