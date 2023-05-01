577e5c9a029b873b4c7e44798c967a2e81e4652d
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-01 16:38:07 +0600

    Login and Register Authentication Done with Email and Password

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -11,8 +11,18 @@ const AuthProvider = ({children}) => {
 
     const user = null;
 
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
     const authInfo ={
         user,
+        createUser,
+        signIn,
     }
 
     return (
